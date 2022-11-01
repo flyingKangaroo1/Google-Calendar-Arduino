37e6026ac9819776f091f1347d5d2cb036b18aff
Suk Min Hwang <92168239+flyingKangaroo1@users.noreply.github.com>
2022-11-01 09:39:16 +0900

    added printCalendar(), nextLine에 한 줄 씩 저장됨

--- ConnectCalendar.ino
+++ ConnectCalendar.ino
@@ -15,7 +15,7 @@ WiFiClientSecure client;
 // select which pin will trigger the configuration portal when set to LOW
 #define TRIGGER_PIN 0
 
-int timeout = 120; // seconds to run for
+int timeout = 80; // seconds to run for
 
 
 // Set the number of entries to show - default is 10 - note that spacing between entries can push some out of the viewable area
@@ -26,7 +26,7 @@ char calendarServer[] = "script.google.com";
 char calendarServer2[] = "script.googleusercontent.com";
 
 // Write the path for your google script to fetch calendar events
-String calendarRequest = "https://script.google.com/macros/s/[고유코드]/exec";
+String calendarRequest = "https://script.google.com/macros/s/[고유코드 입력]/exec";
 
 // Right now the calendarentries are limited to time and title
 struct calendarEntries
@@ -35,10 +35,12 @@ struct calendarEntries
   String calTitle;
 };
 
+String outputStr = "";
 /////////////////////////////////////////////////////////
-//                      FONCTIONS                      //
+//                      FUNCTIONS                      //
 /////////////////////////////////////////////////////////
 bool displayCalendar();
+bool printCalendar();
 String getURL(String url);
 bool getRequest(char *urlServer, String urlRequest);
 /////////////////////////////////////////////////////////
@@ -76,7 +78,9 @@ void loop() {
     //if you get here you have connected to the WiFi
       Serial.println("connected...yeey :)");
 
-      displayCalendar();
+      if (displayCalendar()) {
+        printCalendar();
+      }
     }
   }
   
@@ -93,7 +97,7 @@ String newURL = "";
 
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
-unsigned long period = 5000;
+unsigned long period = 10000;
 
 // Main display code - assumes that the display has been initialized
 bool displayCalendar() {
@@ -107,13 +111,24 @@ bool displayCalendar() {
   }
    
   previousMillis = millis();
-  while(client.connected() || ((currentMillis - previousMillis) < period) )
+  currentMillis = previousMillis;
+  Serial.println(currentMillis - previousMillis);
+  Serial.println( client.connected() );
+  
+  if (!client.connected()) {
+    Serial.println("client1 is not connected");
+    return false;
+  }
+
+  // client이 connected 안 되있거나 또는 10초 이상 동안 돌때 while문 빠져나감
+  while(client.connected() && ((currentMillis - previousMillis) < period) )
   {  
     currentMillis = millis(); 
     Serial.println("1");
     Serial.println(currentMillis - previousMillis);
-    while (client.available())
+    while (client.available() && ((currentMillis - previousMillis) < period) )
     {
+      currentMillis = millis();
       Serial.println("2");
       // Fill the buffer and make a note of the total read length 
       int actualLength = client.read(_buffer, RESP_BUFFER_LENGTH);
@@ -132,14 +147,14 @@ bool displayCalendar() {
 
       if (_responseString.indexOf("HREF=") > 0){
         newURL = getURL(_responseString);
-        Serial.println("new url: " + newURL);     
+        Serial.println("new url: " + newURL);
+        client.stop();     
       }
     }
   }
 
   delete[] _buffer;
   // Clear buffer memory
-  client.stop();
   
   Serial.println("before request");
   
@@ -149,13 +164,24 @@ bool displayCalendar() {
   }
   
   Serial.println("after request");
-  while(client.connected())
+  
+  if (!client.connected()) {
+    Serial.println("client2 is not connected");
+    return false;
+  }
+
+  previousMillis = millis();
+  currentMillis = previousMillis;
+  Serial.println(currentMillis - previousMillis);
+  while( client.connected() && ((currentMillis - previousMillis) < period) )
   { 
+    currentMillis = millis(); 
     Serial.println("3");  
+    Serial.println(currentMillis - previousMillis);
     while (client.available())
     {
       Serial.println("4");
-      String outputStr = client.readString();
+      outputStr = client.readString();
       Serial.println(outputStr);
     }
   }
@@ -165,6 +191,53 @@ bool displayCalendar() {
   
   return true;
 }
+
+
+bool printCalendar() {
+  String tagStr1 = "No events scheduled";
+  String tagStr2 = "Current events:";
+  String tagStr3 = "Events later:";
+  String endStr = "\n";
+
+  int startIndex = outputStr.indexOf(tagStr1);
+  int endIndex = outputStr.indexOf(endStr, startIndex);
+  int lastIndex = outputStr.lastIndexOf(endStr);
+  
+  Serial.println("Print");
+
+  // No events scheduled
+  if ( startIndex != -1 ) {
+    String firstLine = outputStr.substring(startIndex, endIndex);
+    Serial.println(firstLine);
+  }
+
+  // There are events scheduled
+  else {
+    startIndex = outputStr.indexOf(tagStr2);
+    if (startIndex == -1) {
+      startIndex = outputStr.indexOf(tagStr3);
+    }
+
+    String firstLine = outputStr.substring(startIndex, endIndex);
+    Serial.println(firstLine);
+
+    // 마지막 \n 까지 출력을 한다
+    // nextLine이 한 줄임
+    // 홀수줄이 event이름이고 짝수줄이 event시간임
+    while (true) {
+      int endIndex2 = outputStr.indexOf(endStr, endIndex + 1);
+      String nextLine = outputStr.substring(endIndex + 1, endIndex2);
+      Serial.println(nextLine);
+
+      endIndex = endIndex2;
+
+      if (endIndex == lastIndex) {
+        break;
+      }
+    }
+  }
+}
+
 
 // Generic code for getting requests - doing it a bit pure,
 // as most libraries I tried could not handle the redirects from google
@@ -200,8 +273,8 @@ String getURL(String url) {
   String tagStr = "Location: ";
   String endStr = "\n";
 
-  int indexFrom = url.indexOf(tagStr) + tagStr.length();
-  int indexTo   = url.indexOf(endStr, indexFrom);
+  int startIndex = url.indexOf(tagStr) + tagStr.length();
+  int endIndex   = url.indexOf(endStr, startIndex);
   
-  return url.substring(indexFrom, indexTo);
+  return url.substring(startIndex, endIndex);
 }
